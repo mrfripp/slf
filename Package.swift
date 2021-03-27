baaa861bf499b63e1a7d66a7857e66f1917b8e2a
Fredrik Pettersson <fredrik.lars.pettersson@gmail.com>
2021-03-27 13:40:21 +0000

    cleanup

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", .exact("9.0.0"))
+        .package(
+            url: "https://github.com/Quick/Nimble",
+            from: "9.0.0"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +31,10 @@ let package = Package(
         ),
         .testTarget(
             name: "slfTests",
-            dependencies: ["slf", "Nimble"]
+            dependencies: [
+                "slf",
+                "Nimble"
+            ]
         ),
         .target(
             name: "SimpleLogger",
@@ -36,7 +42,10 @@ let package = Package(
         ),
         .testTarget(
             name: "SimpleLoggerTests",
-            dependencies: ["SimpleLogger", "Nimble"]
+            dependencies: [
+                "SimpleLogger",
+                "Nimble"
+            ]
         )
     ]
 )
